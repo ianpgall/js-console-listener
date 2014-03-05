f0852e8b830c11f621caba57d56f2fe15fc64e49
Ian Gall <ianpgall@gmail.com>
2014-03-05 18:10:04 -0500

    Update console-listener.js
    Comments

--- console-listener.js
+++ console-listener.js
@@ -7,6 +7,8 @@
 	A = [];
 	F = function () { return; };
 	O = {};
+
+	// All possible standard methods to provide an interface for
 	consoleMethods = [
 		"assert", "clear", "count", "debug",
 		"dir", "dirxml", "error", "exception",
@@ -15,8 +17,14 @@
 		"table", "time", "timeEnd", "timeStamp",
 		"trace", "warn"
 	];
+
+	// Holds handlers to be executed for every method
 	allHandlers = [];
+
+	// Holds handlers per method
 	methodObj = {};
+
+	// Overrides the existing console methods, to call any stored handlers first
 	fixConsoleMethod = (function () {
 		var func, empty;
 
@@ -25,20 +33,28 @@
 		};
 
 		if (w.console) {
+			// If `console` is even available
 			func = function (methodName) {
 				var old;
 				if (methodName in console && (old = console[methodName])) {
+					// Checks to see if `methodName` is defined on `console` and has valid function to execute
+					// (and stores the old handler)
 					console[methodName] = function () {
+						// Overwrites current console method with this function
 						var args, argsForAll, i, j;
+						// Copy all arguments passed to handler
 						args = A.slice.call(arguments, 0);
 						for (i = 0, j = methodObj[methodName].handlers.length; i < j; i++) {
+							// Loop over all stored handlers for this specific method and call them
 							F.apply.call(methodObj[methodName].handlers[i], console, args);
 						}
 						for (i = 0, j = allHandlers.length; i < j; i++) {
+							// Loop over all stored handlers for ALL events and call them
 							argsForAll = [methodName];
 							A.push.apply(argsForAll, args);
 							F.apply.call(allHandlers[i], console, argsForAll);
 						}
+						// Calls old
 						F.apply.call(old, console, args);
 					};
 				}
@@ -51,9 +67,11 @@
 		return func;
 	}());
 
+	// Loop through all standard console methods and add a wrapper function that calls stored handlers
 	(function () {
 		var i, j, cur;
 		for (i = 0, j = consoleMethods.length; i < j; i++) {
+			// Loop through all valid console methods
 			cur = consoleMethods[i];
 			methodObj[cur] = {
 				handlers: []
@@ -62,26 +80,33 @@
 		}
 	}());
 
+	// Main handler exposed
 	consoleOn = function (methodName, callback) {
 		var key, cur;
 		if (O.toString.call(methodName) === "[object Object]") {
+			// Object literal provided as first argument
 			for (key in methodName) {
+				// Loop through all keys in object literal
+				cur = methodName[key];
 				if (key === "all") {
-					cur = methodName[key];
+					// If targeting all events
 					allHandlers.push(cur);
 				} else if (key in methodObj) {
-					cur = methodName[key];
+					// If targeting specific valid event
 					methodObj[key].handlers.push(cur);
 				}
 			}
 		} else if (typeof methodName === "function") {
+			// Function provided as first argument
 			allHandlers.push(methodName);
 		} else if (methodName in methodObj) {
+			// Valid String event provided
 			methodObj[methodName].handlers.push(callback);
 		}
 	};
 
+	// Actually expose an interface
 	w.ConsoleListener = {
 		on: consoleOn
 	};
-}(window));
+}(this));
